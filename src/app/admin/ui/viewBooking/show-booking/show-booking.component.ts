9267d1b3707e41b72c1b6df1f897e56171a36311
prathmeshns <prathmeshns23@gmail.com>
2024-11-16 10:21:41 +0530

    making admin booking view

--- src/app/admin/ui/viewBooking/show-booking/show-booking.component.ts
+++ src/app/admin/ui/viewBooking/show-booking/show-booking.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { BookingStatusService } from 'src/app/services/booking-status.service';
 
 @Component({
   selector: 'app-show-booking',
   templateUrl: './show-booking.component.html',
   styleUrls: ['./show-booking.component.css']
 })
+
+
 export class ShowBookingComponent {
+
+  constructor(private bookingStatus:BookingStatusService){}
+
+
+
+
+
+  reject() {
+    
+  }
+  approve() {
+
+  }
 
 }
